7a474d7ccb49e7e0c4955cd0cbd751d4aa05260a
Praveen Singh <praveejadoun@yahoo.com>
2019-05-01 01:55:17 +0530

    activeMQ connectivity

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+var Stomp = require('stomp-client');
+const stompit = require('stompit');
+
 const app = express();
 
 const path = require('path');
@@ -6,6 +9,79 @@ const http = require('http');
 const socketIO = require('socket.io');
 
 const port =  5000;
+
+// var MessageConsumer = function MessageConsumer(){};
+
+// MessageConsumer.prototype.init = function init(){
+//     var stompClient = new Stomp('ws://localhost:61614/stomp', 61614, 'guest', 'guest');
+//     stompClient.connect(function(sessionId)
+//     {
+//       stompClient.subscribe('/queue/test', function(body, headers)
+//       {
+//         /*
+//         this callback function is invoked whenever our a client receives a message.
+//         */
+//        console.log("recd message from stomp");
+//       });
+//     });
+    
+//   };
+
+const connectOptions = {
+    'host': 'localhost',
+    'port': 61613,
+    'connectHeaders':{
+      'host': '/stomp',
+      'login': 'guest',
+      'passcode': 'guest',
+      'heart-beat': '5000,5000'
+    }
+  };
+
+
+  stompit.connect(connectOptions, function(error, client) {
+  
+    if (error) {
+      console.log('connect error ' + error.message);
+      return;
+    }
+    
+    const sendHeaders = {
+      'destination': '/queue/test',
+      'content-type': 'text/plain'
+    };
+    
+    const frame = client.send(sendHeaders);
+    frame.write('hello');
+    frame.end();
+    
+    const subscribeHeaders = {
+      'destination': '/queue/test',
+      'ack': 'client-individual'
+    };
+    
+    client.subscribe(subscribeHeaders, function(error, message) {
+      
+      if (error) {
+        console.log('subscribe error ' + error.message);
+        return;
+      }
+      
+      message.readString('utf-8', function(error, body) {
+        
+        if (error) {
+          console.log('read message error ' + error.message);
+          return;
+        }
+        
+        console.log('received message: ' + body);
+        
+        client.ack(message);
+        
+        client.disconnect();
+      });
+    });
+  });
 
 var data= "test data";
 var option = {
@@ -76,4 +152,3 @@ function senddata(socket)
 server.listen(port ,()=>{
     console.log(`Server listenign on port ${port}`);
 });
-
